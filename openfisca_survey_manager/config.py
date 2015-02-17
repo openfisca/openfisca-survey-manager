cf37314a6f4069612d0b0a6c84a6465cdbabcde3
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2015-02-17 08:25:36 +0100

    Move config to separate file

---
+++ openfisca_survey_manager/config.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+
+# OpenFisca -- A versatile microsimulation software
+# By: OpenFisca Team <contact@openfisca.fr>
+#
+# Copyright (C) 2011, 2012, 2013, 2014, 2015 OpenFisca Team
+# https://github.com/openfisca
+#
+# This file is part of OpenFisca.
+#
+# OpenFisca is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# OpenFisca is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import pkg_resources
+from ConfigParser import SafeConfigParser
+
+openfisca_france_data_location = pkg_resources.get_distribution('openfisca-france-data').location
+default_config_files_directory = os.path.join(openfisca_france_data_location)
+
+
+def get_config(config_files_directory = None):
+    if config_files_directory is None:
+        config_files_directory = default_config_files_directory
+
+    parser = SafeConfigParser()
+    config_local_ini = os.path.join(config_files_directory, 'config_local.ini')
+    config_ini = os.path.join(config_files_directory, 'config.ini')
+    parser.read([config_ini, config_local_ini])
+    return parser
