c365fba3559f93e85927d6bb76bd0bad8f71a3fd
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2015-12-22 15:45:12 +0100

    Add default_config_files_directory to in __init__

--- openfisca_survey_manager/__init__.py
+++ openfisca_survey_manager/__init__.py
@@ -1,23 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import os
+import pkg_resources
 
-# Law-to-Code -- Extract formulas & parameters from laws
-# By: Emmanuel Raviart <emmanuel@raviart.com>
-#
-# Copyright (C) 2013 OpenFisca Team
-# https://github.com/openfisca/LawToCode
-#
-# This file is part of Law-to-Code.
-#
-# Law-to-Code is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# Law-to-Code is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+default_config_files_directory = os.path.join(
+    pkg_resources.get_distribution('openfisca-survey-manager').location)
