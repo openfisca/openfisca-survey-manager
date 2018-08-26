f99165bc2efc86b50655261e0879c28edfe5783b
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2018-08-26 22:21:51 +0200

    Fix to run travis test

--- openfisca_survey_manager/__init__.py
+++ openfisca_survey_manager/__init__.py
@@ -8,6 +8,14 @@ import pkg_resources
 
 log = logging.getLogger(__name__)
 
+# Hack to for use at the CASD (shared user)
+# Use taxipp/.config/ directory if exists as default_config_files_directory
+try:
+    taxipp_location = pkg_resources.get_distribution('taxipp').location
+    default_config_files_directory = os.path.join(taxipp_location, '.config', 'openfisca-survey-manager')
+except pkg_resources.DistributionNotFound:
+    default_config_files_directory = None
+
 # Travis tests
 is_travis = 'TRAVIS' in os.environ
 
@@ -18,14 +26,6 @@ if is_travis:
         'tests',
         'data_files',
         )
-
-# Hack to for use at the CASD (shared user)
-# Use taxipp/.config/ directory if exists as default_config_files_directory
-try:
-    taxipp_location = pkg_resources.get_distribution('taxipp').location
-    default_config_files_directory = os.path.join(taxipp_location, '.config', 'openfisca-survey-manager')
-except pkg_resources.DistributionNotFound:
-    default_config_files_directory = None
 
 if default_config_files_directory:
     if not os.path.exists(default_config_files_directory):
