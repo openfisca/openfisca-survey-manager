edbb0292060421227d9c1bd78d0eb0771bd23b3b
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2019-03-16 00:37:05 +0100

    Bump

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ doc_lines = __doc__.split('\n')
 
 setup(
     name = 'OpenFisca-Survey-Manager',
-    version = '0.19.0',
+    version = '0.19.1',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [classifier for classifier in classifiers.split('\n') if classifier],
