e107ef5936b74654bb0c1869c9a45c890b599411
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2019-05-22 19:24:25 +0200

    Test and fix create_data_farme_by_entity

---
+++ openfisca_survey_manager/tests/test_create_data_frame_by_entity.py
@@ -0,0 +1,22 @@
+
+# -*- coding: utf-8 -*-
+
+
+import logging
+
+
+from openfisca_survey_manager.tests.test_scenario import create_randomly_initialized_survey_scenario
+
+log = logging.getLogger(__name__)
+
+
+def test_create_data_frame_by_entity():
+    survey_scenario = create_randomly_initialized_survey_scenario()
+    period = '2017-01'
+    df_by_entity = survey_scenario.create_data_frame_by_entity(variables = ['salary', 'rent'], period = period)
+    salary = survey_scenario.calculate_variable('salary', period = period)
+    rent = survey_scenario.calculate_variable('rent', period = period)
+    for entity, df in df_by_entity.items():
+        assert not df.empty, "{} dataframe is empty".format(entity)
+    assert (df_by_entity['person']['salary'] == salary).all().all()
+    assert (df_by_entity['household']['rent'] == rent).all().all()
