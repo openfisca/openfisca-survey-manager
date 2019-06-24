0cdac7d29ec986cce476af8fb52e4e29afc823c0
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2019-06-24 17:12:31 +0200

    Remove junk

--- openfisca_survey_manager/surveys.py
+++ openfisca_survey_manager/surveys.py
@@ -2,28 +2,6 @@
 # -*- coding: utf-8 -*-
 
 
-# OpenFisca -- A versatile microsimulation software
-# By: OpenFisca Team <contact@openfisca.fr>
-#
-# Copyright (C) 2011, 2012, 2013, 2014, 2015 OpenFisca Team
-# https://github.com/openfisca
-#
-# This file is part of OpenFisca.
-#
-# OpenFisca is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# OpenFisca is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-
 import collections
 import os
 import re
@@ -62,7 +40,7 @@ class Survey(object):
     survey_collection = None
 
     def __init__(self, name = None, label = None, hdf5_file_path = None,
-                 survey_collection = None, **kwargs):
+            survey_collection = None, **kwargs):
         assert name is not None, "A survey should have a name"
         self.name = name
         self.tables = dict()
