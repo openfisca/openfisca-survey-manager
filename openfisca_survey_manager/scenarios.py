65f4b3c5152e2f946174bd47693e6b02dba6d245
Simon Loewe <simonloewe@hotmail.com>
2019-06-25 14:35:39 +0200

    Requested change

--- openfisca_survey_manager/scenarios.py
+++ openfisca_survey_manager/scenarios.py
@@ -754,7 +754,7 @@ class AbstractSurveyScenario(object):
 
         :param data:                Contains the data, or metadata needed to know where to find it.
 
-        :param use_marginal_tax_rate: True to go into marginal effective tax rate computation mode
+        :param use_marginal_tax_rate: Bool, True to go into marginal effective tax rate computation mode
         '''
 
         # When not ``None``, it'll try to get the data for *year*.
