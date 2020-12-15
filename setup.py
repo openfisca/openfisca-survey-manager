8a551885a5a084efa711ee3f05d695daef52df9d
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2020-12-15 18:11:36 +0100

    bump numpy and openfisca-core

--- setup.py
+++ setup.py
@@ -70,9 +70,9 @@ setup(
         'configparser',
         'future',
         'humanize',
-        'numpy >= 1.11, < 1.18',  # https://github.com/openfisca/openfisca-core/issues/924
+        'numpy >= 1.11, < 1.19',
         'tables >= 3.4.4, < 4.0.0',
-        'openfisca-core >=34.2.2,<35.0.0',
+        'openfisca-core >=35.0.0, <36.0.0',
         'pandas >= 0.22',
         'pyxdg',
         'PyYAML',
