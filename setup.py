bdc92eff1ea6c9fade522ebd5367ecbbba240d59
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2021-06-17 11:42:32 +0200

    Aggregates (#206)
    * Aggegates can be exported as html

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ doc_lines = __doc__.split('\n')
 
 setup(
     name = 'OpenFisca-Survey-Manager',
-    version = '0.43',
+    version = '0.44',
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
     classifiers = [classifier for classifier in classifiers.split('\n') if classifier],
