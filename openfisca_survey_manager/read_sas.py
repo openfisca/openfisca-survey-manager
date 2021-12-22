89a97d7abf2de110ff739560f2260746b852037f
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2021-12-22 09:58:08 +0100

    Update read_sas.py

--- openfisca_survey_manager/read_sas.py
+++ openfisca_survey_manager/read_sas.py
@@ -14,7 +14,7 @@ def read_sas(sas_file_path, clean = False):
             from sas7bdat import SAS7BDAT
             data_frame = SAS7BDAT(sas_file_path).to_data_frame()
         except ImportError as e2:
-            log.info("pyreadstat not available trying SAS7BDAT")
+            log.info("Neither pyreadstat nor SAS7BDAT are available")
             print(e1)
             raise(e2)
 
