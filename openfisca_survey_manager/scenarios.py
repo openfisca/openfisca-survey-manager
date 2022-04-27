6babf7aef830b02749a036085975183f944659b9
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2022-04-27 11:45:32 +0200

    Update openfisca_survey_manager/scenarios.py
    Co-authored-by: Nikhil Woodruff <35577657+nikhilwoodruff@users.noreply.github.com>

--- openfisca_survey_manager/scenarios.py
+++ openfisca_survey_manager/scenarios.py
@@ -1598,7 +1598,7 @@ def init_variable_in_entity(simulation, entity, variable_name, series, period):
 
     if enum_variable_imputed_as_enum:
         if series.isnull().any():
-            log.debug('There are {} NaN values for {} non NaN values in variable {}'.format(
+            log.debug('There are {} NaN values ({}% of the array) in variable {}'.format(
                 series.isnull().sum(), series.notnull().sum(), variable_name))
             log.debug('We convert these NaN values of variable {} to {} its default value'.format(
                 variable_name, variable.default_value._name_))
