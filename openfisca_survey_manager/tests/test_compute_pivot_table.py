662fe79e0ca7659c001da56ac99be972b1bc651a
Lola Josseran <l.josseran@pse.local>
2024-01-16 10:48:18 +0100

    test

--- openfisca_survey_manager/tests/test_compute_pivot_table.py
+++ openfisca_survey_manager/tests/test_compute_pivot_table.py
@@ -8,14 +8,14 @@ def test_compute_pivot_table():
 
     return survey_scenario.compute_pivot_table(
         aggfunc = "mean",
-        columns = ['decile'],
+        columns = ['age'],
         difference = False,
         filter_by = None,
         index = None,
         period = period,
         use_baseline = True,
         use_baseline_for_columns = True,
-        values = ['niveau_de_vie'],
+        values = ['salary'],
         missing_variable_default_value = 0,
         concat_axis = None,
         weighted = True,
