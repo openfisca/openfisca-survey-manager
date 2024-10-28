ce72b796f2acff2656ffa6962f4eadc6f632f66e
clallemand <=>
2024-10-28 17:06:29 +0100

    upgrade dependencies

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ setup(
         'chardet >=5.1.0, < 6.0',
         'configparser >= 5.3.0, < 8.0',
         'humanize >=4.6.0, < 5.0',
-        'openfisca-core >=42.0.0, <43.0.0',
+        'openfisca-core >=43.0.0, <44.0.0',
         'pandas >=2.0.3, < 3.0',
         'pyarrow >=13.0.0, < 14.0.0',
         'pyxdg >=0.28, < 0.29',
