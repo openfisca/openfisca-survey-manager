a5fcf57e248ba80baacb427f74f5c0732cfb35cc
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2026-01-23 16:08:45 +0100

    refactor: Migrate os.path to pathlib in config, tests, and scripts

--- openfisca_survey_manager/config.py
+++ openfisca_survey_manager/config.py
@@ -1,5 +1,5 @@
 import configparser
-import os
+from pathlib import Path
 
 
 class Config(configparser.ConfigParser):
@@ -8,14 +8,14 @@ class Config(configparser.ConfigParser):
     def __init__(self, config_files_directory=None):
         configparser.ConfigParser.__init__(self)
         if config_files_directory is not None:
-            config_ini = os.path.join(config_files_directory, "config.ini")
-            assert os.path.exists(config_ini), f"{config_ini} is not a valid path"
+            config_ini = Path(config_files_directory) / "config.ini"
+            assert config_ini.exists(), f"{config_ini} is not a valid path"
             self.config_ini = config_ini
             self.read([config_ini])
 
     def save(self):
         assert self.config_ini, "configuration file path is not defined"
-        assert os.path.exists(self.config_ini)
+        assert self.config_ini.exists()
         config_file = open(self.config_ini, "w")
         self.write(config_file)
         config_file.close()
