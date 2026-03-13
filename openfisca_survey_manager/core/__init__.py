be49efaf94cac40a9010d82efea53df4992ef94a
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2026-03-13 11:18:32 +0100

    Clean docs mention

--- openfisca_survey_manager/core/__init__.py
+++ openfisca_survey_manager/core/__init__.py
@@ -1,6 +1,4 @@
 # Survey, Table, SurveyCollection. Legacy modules re-export for compatibility.
-# See docs/REFACTORING_PLAN.md.
-
 from openfisca_survey_manager.core.dataset import SurveyCollection
 from openfisca_survey_manager.core.survey import NoMoreDataError, Survey
 from openfisca_survey_manager.core.table import Table
